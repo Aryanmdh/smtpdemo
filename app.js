85b6903d5c4ba4178df1d0a1624e91d0f6117948
aryanmdh <mallikarjun@nexweave.co>
2022-06-09 01:09:14 +0530

    test demo

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const SMTPServer = require("smtp-server").SMTPServer;
-const parser = require("mailparser").simpleParser
+const parser = require("mailparser").simpleParser;
 
 const server = new SMTPServer({
+  secure: false,
+  name: "mail.programmaticnft.com",
   onData(stream, session, callback) {
     parser(stream, {}, (err, parsed) => {
-        if (err)
-        console.log("ERR" , err)
-      
-      console.log(parsed)
-      stream.on("end", callback)
-    })
-    
+      if (err) console.log("ERR", err);
+
+      console.log(parsed);
+      stream.on("end", callback);
+    });
   },
-  disabledCommands: ['AUTH']
+  disabledCommands: ["AUTH"],
+  onConnect(session, callback) {
+    console.log(session);
+    return callback(); // Accept the connection
+  }
 });
 
-server.listen(25,'172.31.16.192')
+server.listen(25, "172.31.16.192");
